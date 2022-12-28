26345cd9e727bc66b7e74a8d299d80ed5b774023
Dean035 <deanking.coc@gmail.com>
2022-12-28 17:39:51 +0100

    Datenbank

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print("Hello, World!")
-print("Burak stinkt")
-print("Burak stinkt noch doller")
+import sqlite3
+connection = sqlite3.connect("Datenbank.db")
+cursor = connection.cursor()
+
+
+Tabelle = """CREATE TABLE anmeldung("benutzername TEXT","passwort TEXT");"""
+cursor.execute(Tabelle)
+connection.commit()
+
+Benutzer = "admin"
+Passwort = "admin"
+insert = f"INSERT INTO anmeldung VALUES('{Benutzer}', '{Passwort}');"
+cursor.execute(insert)
+connection.commit()
+connection.close()
