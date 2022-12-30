08f96c4d21e47893d1dacac474c143d146c82a5b
Dean035 <deanking.coc@gmail.com>
2022-12-30 10:48:43 +0100

    Datenbank error fix

--- Datenbank.py
+++ Datenbank.py
@@ -1,16 +1,25 @@
 import sqlite3, os
-connection = sqlite3.connect("Datenbank.db")
-cursor = connection.cursor()
+
+def createdb():
+    connection = sqlite3.connect("Datenbank.db")
+    cursor = connection.cursor()
+
+    Tabelle = """CREATE TABLE anmeldung("benutzername TEXT","passwort TEXT");"""
+    cursor.execute(Tabelle)
+    connection.commit()
+
+    Benutzer = "admin"
+    Passwort = "admin"
+    insert = f"INSERT INTO anmeldung VALUES('{Benutzer}', '{Passwort}');"
+    cursor.execute(insert)
+    connection.commit()
+    connection.close()
+    print("test")
 
 
-Tabelle = """CREATE TABLE anmeldung("benutzername TEXT","passwort TEXT");"""
-cursor.execute(Tabelle)
-connection.commit()
+if os.path.exists("Datenbank.db"):
+    print("Datenbank existiert bereits")
+else:
+    createdb()
 
-Benutzer = "admin"
-Passwort = "admin"
-insert = f"INSERT INTO anmeldung VALUES('{Benutzer}', '{Passwort}');"
-cursor.execute(insert)
-connection.commit()
-connection.close()
-print("test")
+
