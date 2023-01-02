128dc91d3124d80f6a39da2051051db7d6b7a405
Dean035 <deanking.coc@gmail.com>
2023-01-02 15:40:34 +0100

    Register erstellt

--- Seiten.py
+++ Seiten.py
@@ -4,11 +4,37 @@ import Datenbank
 Anmeldung = Tk()
 Anmeldung.title("Anmeldung")
 
+Anmeldung.geometry("400x400")
+Anmeldung.resizable(width=0, height=0)
+
 def Main():
     Mainwindow = Tk()
     Mainwindow. title("Mainwindow")
     Anmeldung.destroy()
 
+def register():
+    Register = Tk()
+    Register.title("Registration")
+    Anmeldung.destroy()
+
+    RgstBenutzername = Label(Register, text="Benutzername")
+    RgstBenutzereingabe = Entry(Register, bd=5, width=40)
+    RgstPasswort = Label(Register, text="Passwort")
+    RgstPassworteingabe = Entry(Register, bd=5, width=40)
+    RgstConfirm = Label(Register, text="Bestätigung des Passworts")
+    RgstConfirmeingabe = Entry(Register, bd=5, width=40)
+    RgstButton = Button(Register, text="Registrieren")
+
+    RgstBenutzername.pack()
+    RgstBenutzereingabe.pack()
+    RgstPasswort.pack()
+    RgstPassworteingabe.pack()
+    RgstConfirm.pack()
+    RgstConfirmeingabe.pack()
+    RgstButton.pack()
+
+Rgstlabel = Label(Anmeldung, text="Wenn Sie noch keinen Account besitzen Drücken sie bitte hier:")
+Rgstbutton = Button(Anmeldung, text="Registration", command=register)
 Benutzernamelabel = Label(Anmeldung, text="Benutzername")
 Anmeldungsbutton = Button(Anmeldung, text="Anmelden", command=Main)
 Benutzernameeingabe = Entry(Anmeldung, bd=5, width= 40)
@@ -18,11 +44,14 @@ Passworteingabe = Entry(Anmeldung, bd=5, width= 40)
 
 
 
+
 Benutzernamelabel.pack()
 Benutzernameeingabe.pack()
 Passwortlabel.pack()
 Passworteingabe.pack()
 Anmeldungsbutton.pack()
-
+Rgstlabel.pack()
+Rgstlabel.place(relx= 0.1, rely=0.8)
+Rgstbutton.place(relx=0.4, rely=0.9)
 Anmeldung.mainloop()
 
