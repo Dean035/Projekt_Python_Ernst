0195efc87ddd080405eafae290942adfa15cb7a0
Dean035 <121295010+Dean035@users.noreply.github.com>
2023-01-26 12:50:50 +0100

    Update Seiten.py

--- Seiten.py
+++ Seiten.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from Datenbank import NewAcc, Ausgabe
+from Datenbank import *
 
 Anmeldung = Tk()
 Anmeldung.title("Anmeldung")
@@ -8,13 +8,12 @@ Anmeldung.geometry("400x400")
 Anmeldung.resizable(width=0, height=0)
 
 
-
 def inpAnmeldung():
     current_input = Benutzernameeingabe.get()
     current_input2 = Passworteingabe.get()
     print(current_input, current_input2)
     Mainwindow = Tk()
-    Mainwindow. title("Mainwindow")
+    Mainwindow.title("Mainwindow")
     Anmeldung.destroy()
     Mainwindow.geometry("650x450")
     Mainwindow.resizable(width=0, height=0)
@@ -28,21 +27,21 @@ def inpAnmeldung():
             Linear.title("Lineare-Funktion")
             Linear.geometry("600x600")
             Linear.resizable(width=0, height=0)
-            butoon1 = Button(Linear,text= "Ausführen",)
-            butoon2 = Button(Linear, text="Beispiele",)
-            butoon3 = Button(Linear, text="Zoom-in",)
-            butoon4 = Button(Linear, text="Zoom-out",)
-            ytext = Entry(Linear, bd=5, width= 7)
-            xtext = Entry(Linear, bd=5, width= 7)
-            btext = Entry(Linear, bd=5, width= 7)
-            mtext = Entry(Linear, bd=5, width= 7)
-            xbeschriftung = Entry(Linear, bd=5, width= 7)
-            ybeschriftung = Entry(Linear, bd=5, width= 7)
+            butoon1 = Button(Linear, text="Ausführen", )
+            butoon2 = Button(Linear, text="Beispiele", )
+            butoon3 = Button(Linear, text="Zoom-in", )
+            butoon4 = Button(Linear, text="Zoom-out", )
+            ytext = Entry(Linear, bd=5, width=7)
+            xtext = Entry(Linear, bd=5, width=7)
+            btext = Entry(Linear, bd=5, width=7)
+            mtext = Entry(Linear, bd=5, width=7)
+            xbeschriftung = Entry(Linear, bd=5, width=7)
+            ybeschriftung = Entry(Linear, bd=5, width=7)
 
             ytext.pack(), xtext.pack(), btext.pack(), mtext.pack(), xbeschriftung.pack(), ybeschriftung.pack(),
             butoon4.pack(), butoon3.pack(), butoon2.pack(), butoon1.pack()
 
-            ytext.place(relx= 2.1, rely=7.8), xtext.place(relx= 4.1, rely=6.8)
+            ytext.place(relx=2.1, rely=7.8), xtext.place(relx=4.1, rely=6.8)
 
 
         elif Auswahl == "Quadratische-Funktion":
@@ -81,33 +80,31 @@ def inpAnmeldung():
             Integral.geometry("400x400")
             Integral.resizable(width=0, height=0)
 
-
-    drop = OptionMenu(Mainwindow, Funktionen,  "Lineare-Funktion" , "Quadratische-Funktion" ,
-                      "Ganzrationale-Funktionen" ,"Trigonometrische-Funktionen" , "Exponential-Funktionen" ,
-                      "Einstieg-Differenzialrechnung" , "Kurvendiskussion", "Integralrechnung", command=show)
-
-
-
-
+    drop = OptionMenu(Mainwindow, Funktionen, "Lineare-Funktion", "Quadratische-Funktion",
+                      "Ganzrationale-Funktionen", "Trigonometrische-Funktionen", "Exponential-Funktionen",
+                      "Einstieg-Differenzialrechnung", "Kurvendiskussion", "Integralrechnung", command=show)
 
     drop.pack()
     drop.place(relx=0.0, rely=0.0)
 
+
 def register():
     Register = Tk()
     Register.title("Registration")
-    #Anmeldung.destroy()
+
+    # Anmeldung.destroy()
 
     def Rgstinp():
         RgstinpBenutzername = RgstBenutzereingabe.get()
         RgstinpPasswort = RgstPassworteingabe.get()
         RgstinpPasswortConfirm = RgstConfirmeingabe.get()
 
-        if RgstinpPasswort == RgstinpPasswortConfirm:
-            print(RgstinpBenutzername, RgstinpPasswort, RgstinpPasswortConfirm)
-            NewAcc(RgstBenutzereingabe.get(), RgstPassworteingabe.get())
-            Ausgabe()
-            Register.destroy()
+        if RgstinpPasswort == RgstinpPasswortConfirm and RgstinpBenutzername != "":
+            if PrüfungRgst(RgstinpBenutzername):
+                print(RgstinpBenutzername, RgstinpPasswort, RgstinpPasswortConfirm)
+                NewAcc(RgstBenutzereingabe.get(), RgstPassworteingabe.get())
+                Ausgabe()
+                Register.destroy()
         else:
             print("Das Passwort und die Bestätigung des Passworts stimmen nicht überein")
             RegisterFail = Tk()
@@ -119,13 +116,12 @@ def register():
                 RegisterFail.destroy()
 
             OkButton = Button(RegisterFail, text="Ok", command=OkButtonClick)
-            LabelFail = Label(RegisterFail, text="Das Passwort und die Bestätigung des Passwortes stimmen nicht überein!")
+            LabelFail = Label(RegisterFail,
+                              text="Das Passwort und die Bestätigung des Passwortes stimmen nicht überein!")
 
             LabelFail.pack()
             OkButton.pack()
 
-
-        
     RgstBenutzername = Label(Register, text="Benutzername")
     RgstBenutzereingabe = Entry(Register, bd=5, width=40)
     RgstPasswort = Label(Register, text="Passwort")
@@ -144,16 +140,13 @@ def register():
     RgstButton.pack()
 
 
-
 Rgstlabel = Label(Anmeldung, text="Wenn Sie noch keinen Account besitzen Drücken sie bitte hier:")
 Rgstbutton = Button(Anmeldung, text="Registration", command=register)
 Benutzernamelabel = Label(Anmeldung, text="Benutzername")
 Anmeldungsbutton = Button(Anmeldung, text="Anmelden", command=inpAnmeldung)
-Benutzernameeingabe = Entry(Anmeldung, bd=5, width= 40)
+Benutzernameeingabe = Entry(Anmeldung, bd=5, width=40)
 Passwortlabel = Label(Anmeldung, text="Passwort")
-Passworteingabe = Entry(Anmeldung, bd=5, width= 40)
-
-
+Passworteingabe = Entry(Anmeldung, bd=5, width=40)
 
 Benutzernamelabel.pack()
 Benutzernameeingabe.pack()
@@ -161,7 +154,6 @@ Passwortlabel.pack()
 Passworteingabe.pack()
 Anmeldungsbutton.pack()
 Rgstlabel.pack()
-Rgstlabel.place(relx= 0.1, rely=0.8)
+Rgstlabel.place(relx=0.1, rely=0.8)
 Rgstbutton.place(relx=0.4, rely=0.9)
 Anmeldung.mainloop()
-
