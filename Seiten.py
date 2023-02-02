08dd334c8d951234f2deef4376f5cd0e77cf8b6c
Obitobi-Uchiha <121295990+Obitobi-Uchiha@users.noreply.github.com>
2023-02-02 11:14:53 +0100

    Update Seiten.py

--- Seiten.py
+++ Seiten.py
@@ -2,7 +2,7 @@ from tkinter import *
 from Datenbank import *
 import matplotlib.pyplot as ppt
 import numpy as np
-
+import math as mt
 
 Anmeldung = Tk()
 Anmeldung.title("Anmeldung")
@@ -15,26 +15,40 @@ def inpAnmeldung():
     current_input = Benutzernameeingabe.get()
     current_input2 = Passworteingabe.get()
     print(current_input, current_input2)
-    Mainwindow = Tk()
-    Mainwindow.title("Mainwindow")
-    Anmeldung.destroy()
-    Mainwindow.geometry("650x450")
-    Mainwindow.resizable(width=0, height=0)
-    Funktionen = StringVar(Mainwindow)
-    Funktionen.set("Funktionen")
+
+    def OkButtonClick():
+        Erroranm.destroy()
+
+    if Prüfanm(current_input, current_input2):
+        Mainwindow = Tk()
+        Mainwindow.title("Mainwindow")
+        Anmeldung.withdraw()
+        Mainwindow.geometry("650x450")
+        Mainwindow.resizable(width=0, height=0)
+        Funktionen = StringVar(Mainwindow)
+        Funktionen.set("Funktionen")
+    else:
+        Erroranm = Tk()
+        Erroranm.title("Error")
+        Erroranm.geometry("500x50")
+        Erroranm.resizable(width=0, height=0)
+        OkButton = Button(Erroranm, text="Ok", command=OkButtonClick)
+        LabelFail = Label(Erroranm,
+                          text="Dieser Account existiert nicht! Bitte versuchen sie es erneut")
+
+        LabelFail.pack()
+        OkButton.pack()
 
 
     def show(selection):
 
         Auswahl = selection
-        if Auswahl == "Lineare-Funktion": #erstellt die seite usw
+        if Auswahl == "Lineare-Funktion":
             fenster = Tk()
             fenster.title("Lineare-Funktion")
             fenster.geometry("800x600")
             fenster.resizable(width=0, height=0) #macht das die seite nicht vergrößert werden kann
 
-           # butoon3 = Button(fenster, text="Zoom-in", )
-           # butoon4 = Button(fenster, text="Zoom-out", )
             bisle = Entry(fenster, bd=5, width=12)  # wie weit y geht (bis) start = 0 (textboxen)
             lab1=Label(fenster,text="bis" )
             lab1.pack()
@@ -109,27 +123,92 @@ def inpAnmeldung():
             vonle.pack(), bisle.pack(), btexte.pack(), mtexte.pack(), xbeschriftung.pack(), ybeschriftung.pack(),
             butoon2.pack(), butoon1.pack() #zeigt die scheiße an
 
-            ## butoon3.place(x=100, y=555), butoon4.place(x=180, y=555)
 
             bisle.place(x=7, y=460), vonle.place(x=7, y=400), btexte.place(x=7, y=340), mtexte.place(x=7, y=280),
             xbeschriftung.place(x=7, y=230), ybeschriftung.place(x=7, y=170), butoon1.place(x=7, y=40),
             butoon2.place(x=7, y=555) #wo stehen die einzelen buttons usw
-
 
         elif Auswahl == "Quadratische-Funktion":
             Quadratisch = Tk()
             Quadratisch.title("Quadratische-Funktion")
             Quadratisch.geometry("400x400")
             Quadratisch.resizable(width=0, height=0)
+
         elif Auswahl == "Ganzrationale-Funktionen":
             Ganzrational = Tk()
             Ganzrational.title("Ganzrationale-Funktionen")
-            Ganzrational.geometry("400x400")
+            Ganzrational.geometry("800x600")
             Ganzrational.resizable(width=0, height=0)
+
+            bisge = Entry(Ganzrational, bd=5, width=12)  # wie weit y geht (bis) start = 0 (textboxen)
+            lab1 = Label(Ganzrational, text="bis")
+            lab1.pack()
+            lab1.place(x=7, y=435)
+            bisge.place(x=7, y=460)
+
+            vonge = Entry(Ganzrational, bd=5, width=12)  # wie weit x geht (textboxen)
+            lab2 = Label(Ganzrational, text="von")
+            lab2.pack()
+            lab2.place(x=7, y=375)
+            vonge.place(x=7, y=400)
+
+            ng = Entry(Ganzrational, bd=5, width=12)  # n (textboxen)
+            lab3 = Label(Ganzrational, text="n")
+            lab3.pack()
+            lab3.place(x=7, y=315)
+            ng.place(x=7, y=280)
+
+            ag = Entry(Ganzrational, bd=5, width=12)  # a (textboxen)
+            lab4 = Label(Ganzrational, text="a")
+            lab4.pack()
+            lab4.place(x=7, y=255)
+            ag.place(x=7, y=340)
+
+            xbeschriftungg = Entry(Ganzrational, bd=5, width=12)  # name für xseite (textboxen)
+            lab5 = Label(Ganzrational, text="Name-X")
+            lab5.pack()
+            lab5.place(x=7, y=205)
+            xbeschriftungg.place(x=7, y=230)
+
+            ybeschriftungg = Entry(Ganzrational, bd=5, width=12)  # name für yseite (textboxen)
+            lab6 = Label(Ganzrational, text="Name-Y")
+            lab6.pack()
+            lab6.place(x=7, y=145)
+            ybeschriftungg.place(x=7, y=170)
+
+
+            def Joe_mama():
+                ppt.title("Ganzrationale-Funktionen")
+                ppt.ylabel(ybeschriftung.get())
+                ppt.xlabel(xbeschriftung.get())
+                bisg = bisge.get()
+                vong = vonge.get()
+                age = ag.get()
+                nge = ng.get()
+                nge = float(nge)
+                age = float(age)
+                bisg = float(bisg)
+                vong = float(vong)
+
+                ax = ppt.gca()
+                ppt.gca().set_aspect('equal')
+
+                ax.set_xlim([vong, bisg])
+                ax.set_ylim([vong, bisg])
+
+            buton1 = Button(Ganzrational, text="Ausführen",)#command=Joe_mama)#buttons
+            buton1.place(x=7, y=40)
+            buton2 = Button(Ganzrational, text="Beispiele",) #command=)
+            buton2.place(x=7, y=555)
+
+            vonge.pack(), bisge.pack(), ng.pack(), ag.pack(), xbeschriftungg.pack(), ybeschriftungg.pack(),
+            buton2.pack(), buton1.pack()  # zeigt die scheiße an
+
+
         elif Auswahl == "Trigonometrische-Funktionen":
             Trigonomisch = Tk()
             Trigonomisch.title("Trigonometrische-Funktionen")
-            Trigonomisch.geometry("400x400")
+            Trigonomisch.geometry("800x600")
             Trigonomisch.resizable(width=0, height=0)
         elif Auswahl == "Exponential-Funktionen":
             Exponential = Tk()
@@ -153,9 +232,17 @@ def inpAnmeldung():
             Integral.resizable(width=0, height=0)
 
     drop = OptionMenu(Mainwindow, Funktionen, "Lineare-Funktion", "Quadratische-Funktion",
-                      "Ganzrationale-Funktionen", "Trigonometrische-Funktionen", "Exponential-Funktionen",#dropdown menü
+                      "Ganzrationale-Funktionen", "Trigonometrische-Funktionen", "Exponential-Funktionen",
                       "Einstieg-Differenzialrechnung", "Kurvendiskussion", "Integralrechnung", command=show)
 
+    def logout():
+        Mainwindow.destroy()
+        Anmeldung.deiconify()
+
+
+    logout = Button(Mainwindow, text="Abmelden", command=logout)
+    logout.pack()
+    logout.place(relx=0.9, rely=0.0)
     drop.pack()
     drop.place(relx=0.0, rely=0.0)
 
@@ -177,7 +264,19 @@ def register():
                 NewAcc(RgstBenutzereingabe.get(), RgstPassworteingabe.get())
                 Ausgabe()
                 Register.destroy()
+            else:
+                def OkButtonClick():
+                    ErrorRgst.destroy()
+                ErrorRgst = Tk()
+                ErrorRgst.title("Error")
+                ErrorRgst.geometry("500x50")
+                ErrorRgst.resizable(width=0, height=0)
+                OkButton = Button(ErrorRgst, text="Ok", command=OkButtonClick)
+                LabelFail = Label(ErrorRgst,
+                                  text="Dieser Benutzername existiert bereits! Bitte wählen Sie einen anderen!")
 
+                LabelFail.pack()
+                OkButton.pack()
         else:
             print("Das Passwort und die Bestätigung des Passworts stimmen nicht überein")
             RegisterFail = Tk()
@@ -213,6 +312,7 @@ def register():
     RgstButton.pack()
 
 
+
 Rgstlabel = Label(Anmeldung, text="Wenn Sie noch keinen Account besitzen Drücken sie bitte hier:")
 Rgstbutton = Button(Anmeldung, text="Registration", command=register)
 Benutzernamelabel = Label(Anmeldung, text="Benutzername")
@@ -230,4 +330,3 @@ Rgstlabel.pack()
 Rgstlabel.place(relx=0.1, rely=0.8)
 Rgstbutton.place(relx=0.4, rely=0.9)
 Anmeldung.mainloop()
-
